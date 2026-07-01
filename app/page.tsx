cb7f3077a2699ec0dc88e2043f33e1f26b1dd92d
ignaciohplt <ignaciohipolitoih@gmail.com>
2026-07-01 00:26:38 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import Dashboard from "@/components/Dashboard";
+import LoginGate from "@/components/LoginGate";
 
 export default function Home() {
-  return <Dashboard />;
+  return (
+    <LoginGate>
+      <Dashboard />
+    </LoginGate>
+  );
 }
